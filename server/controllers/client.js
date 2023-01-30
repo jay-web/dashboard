50643b3ab7d135330c1c3e27fc0b7b929f84397e
jay-web <jay.developer12@gmail.com>
2023-01-30 16:19:10 +0530

    Added route in client file for transaction route and created related controller i.e getTransactions from database with filter option on server side as per query

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const ProductStat = require("../models/ProductStat");
 const User = require("../models/User");
+const Transaction = require("../models/Transaction");
 
 exports.getProducts = async (req, res) => {
     try{
@@ -31,6 +32,49 @@ exports.getCustomers = async (req, res) => {
         const customers = await User.find({ role: 'user'}).select("-password");
 
         res.status(200).json(customers);
+
+    }catch(error){
+        res.status(404).json({ message: error.message})
+    }
+}
+
+exports.getTransactions = async (req, res) => {
+    try{
+        // ? Extract the filters if any
+        const { page=1, pageSize=20, sort=null, search="" } = req.query;
+
+        // ? Format the sort for mongodb
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field] : (sortParsed.sort = 'asc' ? 1 : -1)
+            }
+            return sortFormatted;
+        }
+
+        const sortFormatted = Boolean(sort) ?generateSort() : {};
+
+        // ? Call the query to fetch transactions
+        const transactions = await Transaction.find({
+            $or: [
+                { cost : {$regex: new RegExp(search, "i")}},
+                { userId: { $regex: new RegExp(search, "i")}}
+            ]
+
+        })
+        .sort(sortFormatted)
+        .skip(page * pageSize)
+        .limit(pageSize)
+
+        // ? Call the query to fetch number of documents
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: "i"}
+        })
+
+        res.status(200).json({
+            transactions,
+            total
+        });
 
     }catch(error){
         res.status(404).json({ message: error.message})
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {getProducts, getCustomers} = require("../controllers/client")
+const {getProducts, getCustomers, getTransactions} = require("../controllers/client")
 
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.get('/customers', getCustomers);
+router.get('/transactions', getTransactions);
 
 module.exports = router;
