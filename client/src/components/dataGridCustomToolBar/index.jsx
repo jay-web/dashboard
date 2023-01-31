eef09dded7787a1e89d2c90a74118f46c4b547fc
jay-web <jay.developer12@gmail.com>
2023-01-31 18:04:56 +0530

    Added DataGridCustomToolbar component to pass into DataGrid as a component and provide functionality of COLUMNS, DENSITY, EXPORT AND SEARCH on table and passed the same in datagrid table

--- client/src/components/dataGridCustomToolBar/index.jsx
+++ client/src/components/dataGridCustomToolBar/index.jsx
@@ -1,9 +1,44 @@
+import { Search } from "@mui/icons-material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport } from "@mui/x-data-grid";
+import FlexBetween from "components/flexBetween";
 import React from "react";
 
 
-const DataGridCustomToolBar = () => {
+const DataGridCustomToolBar = ({ state, setState }) => {
     return (
-        <div></div>
+        <GridToolbarContainer >
+            <FlexBetween width="100%">
+                <FlexBetween >
+                    <GridToolbarColumnsButton />
+                    <GridToolbarDensitySelector />
+                    <GridToolbarExport />
+
+                </FlexBetween>
+                <TextField 
+                    label="Search by User Id"
+                    sx={{ marginBottom:"0.5rem", width:"15rem"}}
+                    onChange={(e) => setState({...state, searchInput: e.target.value})}
+                    value={state.searchInput}
+                    variant="standard"
+                    InputProps={
+                        { endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton 
+                                    onClick={(() => {
+                                        setState({...state, search: state.searchInput, searchInput: "" })
+                                    
+                                    })}
+                                >
+                                    <Search />
+                                </IconButton>
+                                
+                            </InputAdornment>
+                        )}
+                    }
+                />
+            </FlexBetween>
+        </GridToolbarContainer>
     )
 }
 
--- client/src/components/transactions/index.jsx
+++ client/src/components/transactions/index.jsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import DataGridCustomToolBar from "components/dataGridCustomToolBar";
 import Header from "components/header";
 import React, { useState } from "react";
 import { useGetTransactionsQuery } from "reduxStore/api";
@@ -8,12 +9,13 @@ const initialFilterState = {
     page: 0,
     pageSize: 20,
     sort: {},
-    search: ""
+    search: "",
+    searchInput: ""
 };
 
 const Transactions = () => {
     const [state, setState ] = useState(initialFilterState);
-    
+    const theme = useTheme();
     const {data, isLoading} = useGetTransactionsQuery({
         page: state.page,
         pageSize: state.pageSize,
@@ -53,13 +55,39 @@ const Transactions = () => {
     ]
     return (<Box margin="1.5rem 2.5rem">
         <Header title="TRANSACTIONS" subtitle="Entire list of Transactions" />
-        <Box height="80vh">
+        <Box height="80vh"
+            sx={{
+                "& .MuiDataGrid-root": {
+                  border: "none",
+                },
+                "& .MuiDataGrid-cell": {
+                  borderBottom: "none",
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                  backgroundColor: theme.palette.background.alt,
+                  color: theme.palette.secondary[100],
+                  borderBottom: "none",
+                },
+                "& .MuiDataGrid-virtualScroller": {
+                  backgroundColor: theme.palette.primary.light,
+                },
+                "& .MuiDataGrid-footerContainer": {
+                  backgroundColor: theme.palette.background.alt,
+                  color: theme.palette.secondary[100],
+                  borderTop: "none",
+                },
+                "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                  color: `${theme.palette.secondary[200]} !important`,
+                },
+              }}
+            >
             <DataGrid 
                 loading={isLoading}
                 getRowId={(rows) => rows._id}
                 rows={(data && data.transactions) || []}
                 columns={columns}
                 rowCount={(data && data.total) || 0}
+                rowsPerPageOptions={[20, 50, 100]}
                 paginations
                 page={state.page}
                 pageSize={state.pageSize}
@@ -68,7 +96,10 @@ const Transactions = () => {
                 onPageChange={(newPage) => setState({...state, page:newPage }) }
                 onPageSizeChange ={(newPageSize) => setState({...state, pageSize:newPageSize }) }
                 onSortModelChange={(newSortModel) => setState({...state, sort: newSortModel}) }
-                
+                components={{ Toolbar: DataGridCustomToolBar }}
+                componentsProps={{
+                    toolbar: {state,  setState }
+                }}
             />
         </Box>
 
