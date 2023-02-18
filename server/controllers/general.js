12f1fc8cddf62516fb177e4ce3240ddb53210abb
jay-web <jay.developer12@gmail.com>
2023-02-18 17:58:24 +0530

    Added getDashboardStats function to fetch data for dashboard in general controller server side and create '/dashboard' route to call this function

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const Transaction = require("../models/Transaction");
+const OverallStat = require("../models/OverallStat");
 
 const getUser = async (req, res) => {
     try {
@@ -11,6 +13,43 @@ const getUser = async (req, res) => {
     }
 }
 
-module.exports = {
-    getUser
+const getDashboardStats = async (req, res) => {
+    try {
+        // hard coded values due to mock data
+        const currentMonth = 'December';
+        const currentYear = 2021;
+        const currentDay = "2021-12-15";
+
+        // Recent transaction
+        const transactions = await Transaction.find().limit(50).sort({ createdOn: -1 })
+
+        // Overall stat
+        const overallStat = await OverallStat.find({ year: currentYear});
+
+        const {totalCustomers, yearlyTotalSoldUnits, yearlySalesTotal, monthlyData, salesByCategory } = overallStat[0];
+
+        const thisMonthStats = overallStat[0].monthlyData.find(({ month}) => {
+            return month === currentMonth;
+        });
+
+        const todayStats = overallStat[0].dailyData.find(({ date }) => {
+            return date === currentDay;
+        });
+
+        res.status(200).json({ 
+            totalCustomers, yearlyTotalSoldUnits, yearlySalesTotal, monthlyData, salesByCategory, thisMonthStats, 
+            todayStats, transactions
+        })
+
+
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
 }
+
+module.exports = {
+    getUser,
+    getDashboardStats
+}
+
--- server/routes/general.js
+++ server/routes/general.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {getUser} = require("../controllers/general");
+const {getUser, getDashboardStats} = require("../controllers/general");
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
 
 module.exports = router;
